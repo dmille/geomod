de2f46f78da3ce28e5f5227fa2457b3e194ec79b
david <davidmiller252@gmail.com>
2018-05-14 18:14:23 +0200

    branch fixes

--- A2/david/get_midpoints.m
+++
@@ -1,9 +0,0 @@
-function [X_new, Y_new] = get_midpoints(X, Y)
-%GET_MIDPOINTS Summary of this function goes here
-%   Detailed explanation goes here
-    X_shift = circshift(X, 1);
-    Y_shift = circshift(Y, 1);
-    X_new = 1/2 * (X + X_shift);
-    Y_new = 1/2 * (Y + Y_shift);
-end
-
--- A2/david/power_matrix.m
+++
@@ -1,9 +0,0 @@
-function [P] = power_matrix(X, max_degree)
-%POWER_MATRIX Summary of this function goes here
-%   Detailed explanation goes here
-    P = [];
-    for i = 0:max_degree
-        P = [P (X .^ i)];
-    end
-end
-
