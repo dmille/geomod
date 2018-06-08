060ce5826c07d1f1c0bbac78efe2a03ebb2ea3f9
david <davidmiller252@gmail.com>
2018-06-08 20:46:06 +0200

    Slider Label updates

--- A6/slider_update.m
+++ A6/slider_update.m
@@ -3,17 +3,15 @@ function slider_update(varargin)
 %global myhandles;
 myhandles = guidata(gcbo);
 
-%% Read t value
+%% Read n_beziers value
 myhandles.n_beziers = floor(get(myhandles.n_beziers_slider,'Value'));
-set(myhandles.n_beziers_slider,'Value',myhandles.n_beziers); %update the value in handles
-% display current value in the text box
-%set(myhandles.n_beziers_slider_text,'String',sprintf('t=%4.2f',myhandles.t)); 
 
-%% make the text box follow the slider :)
-%txtpos=get(myhandles.t_slider_text,'position');
-sliderpos=get(myhandles.n_beziers_slider,'position');
-%txtpos(1)=sliderpos(1)+myhandles.n_beziers*(sliderpos(3)-.05);
-%set(myhandles.t_slider_text,'position',txtpos);
+%% update the value in handles
+set(myhandles.n_beziers_slider,'Value',myhandles.n_beziers); 
+
+%% display current value in the text box
+set(myhandles.n_beziers_slider_text,'String',sprintf('Number of Beziers: %d',myhandles.n_beziers)); 
+
 %% update plot
 myhandles.bezier_controlPts = fit_bezier(myhandles.noisy_data, myhandles.n_beziers)
 
